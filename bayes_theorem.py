ad93ee4f4c345a0318aaf74be8ab66b7be4354f7
Henry Pozzetta <henry.pozzetta+git@gmail.com>
2024-07-09 14:55:45 -0400

    Initial commit with Bayes' theorem implementation

---
+++ bayes_theorem.py
@@ -0,0 +1,40 @@
+# bayes_theorem.py
+
+class ProbabilityStrategy:
+    def calculate_prior(self, data):
+        raise NotImplementedError("calculate_prior method not implemented")
+
+    def calculate_likelihood(self, data):
+        raise NotImplementedError("calculate_likelihood method not implemented")
+
+    def calculate_marginal(self, data):
+        raise NotImplementedError("calculate_marginal method not implemented")
+
+class ExampleProbabilityStrategy(ProbabilityStrategy):
+    def calculate_prior(self, data):
+        return data['disease_count'] / data['total_population']
+
+    def calculate_likelihood(self, data):
+        return data['true_positive_count'] / data['disease_count']
+
+    def calculate_marginal(self, data):
+        return data['test_positive_count'] / data['total_population']
+
+def bayes_theorem(data, strategy: ProbabilityStrategy):
+    P_A = strategy.calculate_prior(data)
+    P_B_given_A = strategy.calculate_likelihood(data)
+    P_B = strategy.calculate_marginal(data)
+    return (P_B_given_A * P_A) / P_B
+
+# Example usage:
+if __name__ == "__main__":
+    data = {
+        'total_population': 10000,
+        'disease_count': 100,
+        'test_positive_count': 600,
+        'true_positive_count': 99
+    }
+
+    strategy = ExampleProbabilityStrategy()
+    result = bayes_theorem(data, strategy)
+    print(result)  # Output will be the posterior probability
